b19e1ca2363cefc5eeaf2c41d56b75153c053cc0
Filip Jonassen <filip@pc166-238.hiof.no>
2015-02-03 08:50:04 +0100

    added variables module with comments

--- variables.py
+++ variables.py
@@ -1,2 +1,11 @@
+# Added extra variables for print, Age er et heltall
 age = 14
+# number er et desimaltall
+number = 13.5
+# text er blitt en tekststreng
+text = "Dette er en tekst streng/string"
 print age
+print number
+# text er blitt heltall
+text = 14
+print text
